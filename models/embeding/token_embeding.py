6dd2c63d66b255218a9d7b39ee66938c5d03455c
coungdev <coungdev@gmail.com>
2025-03-05 14:16:06 +0700

    refactor TokenEmbeding to inherit from nn.Embedding

--- models/embeding/token_embeding.py
+++ models/embeding/token_embeding.py
@@ -1,10 +1,10 @@
 from torch import nn
 import math
 
-class TokenEmbeding(nn.Module):
+class TokenEmbeding(nn.Embedding):
     def __init__(self, vocab_size, d_model):
         super(TokenEmbeding, self).__init__(vocab_size, d_model)
-        self.embed = nn.Embedding(vocab_size, d_model)
-    
+        self.d_model = d_model
+        
     def forward(self, x):
-        return self.embed(x) * math.sqrt(self.d_model)
+        return super().forward(x) * math.sqrt(self.d_model)

3959f92e98edf0a54cae2191dbe14ef2cc46bbb7
coungdev <coungdev@gmail.com>
2025-03-05 15:11:23 +0700

    add TransformerEmbeding class for token and positional embeddings

---
+++ models/embeding/transformer_embeding.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+from token_embeding import TokenEmbeding
+from positional_embeding import PositionalEmbeding
+
+class TransformerEmbeding(nn.Module):
+    """
+    Transformer Embeding class.
+    
+    Args:
+    vocab_size: int, the size of the vocabulary.
+    d_model: int, the dimension of the model.
+    max_len: int, the maximum length of the sequence.
+    
+    Returns:
+    torch.Tensor, the transformer embeding shape of (b, seq_len, d_model).
+    """
+    
+    def __init__(self, vocab_size : int, d_model : int, max_len : int) -> None:
+        super(TransformerEmbeding, self).__init__()
+        self.token_embeding = nn.Embedding(vocab_size, d_model)
+        self.positional_embeding = PositionalEmbeding(max_len, d_model)
+        
+        
+    def forward(self, x : torch.Tensor) -> torch.Tensor:
+        return nn.Dropout(p=0.1)(self.token_embeding(x) + self.positional_embeding(x))

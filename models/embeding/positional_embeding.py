4c6ef6b4c7395ce056850f4ea3a853986fc56504
coungdev <coungdev@gmail.com>
2025-03-05 14:57:30 +0700

    add PositionalEmbeding class and improve TokenEmbeding documentation

---
+++ models/embeding/positional_embeding.py
@@ -0,0 +1,31 @@
+import torch
+from torch import nn
+
+class PositionalEmbeding(nn.Module):
+    """
+    Positional Embeding class.
+    
+    Args:
+    max_len: int, the maximum length of the sequence.
+    d_model: int, the dimension of the model.
+    
+    Returns:
+    torch.Tensor, the positional embeding shape of (b, seq_len, d_model).
+    """
+    def __init__(self, max_len, d_model):
+        super(PositionalEmbeding, self).__init__()
+        self.d_model = d_model
+        self.pe = torch.zeros(max_len, d_model)
+        
+        pos = torch.arange(0, max_len).unsqueeze(1).float()
+        _2i = torch.arange(0, d_model, step=2).float()
+        div_term = torch.exp(10000, _2i / d_model)
+        
+        self.pe[:, 0::2] = torch.sin(pos / div_term)
+        self.pe[:, 1::2] = torch.cos(pos / div_term)
+    
+    
+    def forward(self, x):
+        batch_size, seq_len = x.size()
+        
+        return self.pe[:seq_len, :].unsqueeze(0).expand(batch_size, -1, -1)
--- models/embeding/token_embeding.py
+++ models/embeding/token_embeding.py
@@ -2,6 +2,17 @@ from torch import nn
 import math
 
 class TokenEmbeding(nn.Embedding):
+    """
+    Token Embeding class.
+    
+    Args:
+    vocab_size: int, the size of the vocabulary.
+    d_model: int, the dimension of the model.
+    
+    Returns:
+    torch.Tensor, the token embeding shape of (b, seq_len, d_model).
+    """
+    
     def __init__(self, vocab_size, d_model):
         super(TokenEmbeding, self).__init__(vocab_size, d_model)
         self.d_model = d_model

fa9a05b71c99a95f071c609cc223cc30d34aa290
coungdev <coungdev@gmail.com>
2025-03-06 10:54:37 +0700

    refactor Encoder to add separate LayerNorm and Dropout layers for improved clarity and modularity

--- models/blocks/encoder.py
+++ models/blocks/encoder.py
@@ -8,18 +8,20 @@ class Encoder(nn.Module):
     def __init__(self, d_model=512, num_heads=8) -> None:
         super(Encoder, self).__init__()
         self.MHA = MultiHeadAttention(d_model=d_model, num_heads=num_heads)
-        self.layer_norm = LayerNorm(d_model)
+        self.norm1 = LayerNorm(d_model)
+        self.norm2 = LayerNorm(d_model)
         self.FFN = FFN(d_model=d_model, d_ff=d_model*4)
-        self.dropout = nn.Dropout(0.1)
+        self.dropout1 = nn.Dropout(0.1)
+        self.dropout2 = nn.Dropout(0.1)
         
     def forward(self, x : torch.Tensor) -> torch.Tensor:
         z = self.MHA(x, x, x)
-        z = self.dropout(z)
-        z = self.layer_norm(z + x)
+        z = self.dropout1(z)
+        z = self.norm1(z + x)
         
         out = self.FFN(z)
-        out = self.dropout(out)
-        out = self.layer_norm(out + z)
+        out = self.dropout2(out)
+        out = self.norm2(out + z)
         
         return out
         
